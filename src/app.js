02f196bdb3f825d8684e96d2512bf53f794811c2
Halil Ibrahim Karagöz <halil.karagoz@a101.com.tr>
2022-03-07 17:08:27 +0300

    Buddy first version

--- src/app.js
+++ src/app.js
@@ -1,1 +1,42 @@
-console.log("Test");
+import axios from 'axios'
+import { people } from './people.js';
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function formatPairs(pairs) {
+
+    let fS = "";
+
+    for (const pair of pairs) {
+        fS += `${pair[0]} <---> ${pair[1]}\n\n`
+    }
+
+    return fS;
+}
+
+function createPairs(people) {
+    if (people.length % 2 === 1) {
+        let rmindex = getRandomInt(people.length);
+        people.splice(rmindex, 1);
+    }
+
+    let pairs = [];
+
+    while (people.length > 0) {
+        let pair1 = people[0];
+        people.splice(0, 1);
+        let pair2 = getRandomInt(people.length);
+        pairs.push([pair1, people[pair2]]);
+        people.splice(pair2, 1);
+    }
+
+    axios.post("https://hooks.slack.com/services/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", { "text": formatPairs(pairs) })
+        .then((res) => { console.log(res) })
+        .catch((err) => { console.log(err) });
+
+}
+
+createPairs(people);
---
+++ src/people.js
@@ -0,0 +1,15 @@
+export let people = [
+    "Halil",
+    "Hasan",
+    "Burak",
+    "Evren",
+    "Beyza",
+    "Mustafa",
+    "Mehmet",
+    "Enes",
+    "Murat",
+    "Ali",
+    "İlayda",
+    "Talha",
+    "Kağan"
+]
